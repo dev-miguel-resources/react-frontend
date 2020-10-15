a5732294d727cd4c05fcc47775f611d3902d9f07
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-10-15 07:39:21 -0300

    definición de componentes, variables, rutas y patrón de diseño HOC

---
+++ src/auth/AdminRoute.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAuthenticated } from "./index";
+
+//HOC: Pattern High Order Component
+const AdminRoute = ({ component: Component, ...rest }) => (
+    <Route 
+        {...rest}
+        render={props => 
+            isAuthenticated() && isAuthenticated().user.role === 1 ? (
+                <Component {...props} />
+            ) : (
+                <Redirect 
+                    to={{
+                        pathname: "/signin",
+                        state: { from: props.location }
+                    }}
+                />
+            ) 
+        }
+    />
+);
+
+export default AdminRoute;
